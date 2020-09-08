09936051cf1b8f27aed2bfeb5b90286f0d3c08f3
R ansh joseph <70263460+anshjoseph@users.noreply.github.com>
2020-09-08 13:35:05 +0530

    Delete gitstart.bat

--- gitstart.bat
+++
@@ -1,7 +0,0 @@
-echo "# hroku_lab_atten" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/anshjoseph/hroku_lab_atten.git
-git push -u origin master
